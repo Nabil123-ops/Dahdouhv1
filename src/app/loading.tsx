3bbcffe46721d1b92c25466ce2f61c59a125f625
Nabil123-ops <dahdouhn74@gmail.com>
2025-11-21 18:20:19 +0200

    Update loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { FcGoogle } from 'react-icons/fc'
 
 const Loading = () => {
   return (
     <section className='h-full w-full grid place-content-center'>
       <div className='flex items-center justify-center gap-3 text-8xl'>
-      <FcGoogle />
+        <img 
+          src="/logo.jpg" 
+          alt="Dahdouh AI"
+          className="w-20 h-20 object-contain"
+        />
       </div>
+
       <div className="loader" />
     </section>
   )
