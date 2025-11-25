2714f766dc74d3e753f957f8b4a50ee3986f1996
Nabil123-ops <dahdouhn74@gmail.com>
2025-11-25 12:43:55 +0200

    Update signin-now.tsx

--- src/components/header-components/signin-now.tsx
+++ src/components/header-components/signin-now.tsx
@@ -3,32 +3,17 @@
 import DevButton from "@/components/dev-components/dev-button";
 import Image from "next/image";
 import { GoSignOut } from "react-icons/go";
-import { IoApps } from "react-icons/io5";
 import { signIn, signOut } from "next-auth/react";
 import DevPopover from "../dev-components/dev-popover";
 import ReactTooltip from "../dev-components/react-tooltip";
-import PortfolioProjects from "./portfolio-projects";
 import CustomGeminiKey from "./custom-geminikey";
 
 export default function SignInNow({ userData }: any) {
-
   return (
     <div className="flex items-center gap-2">
       <CustomGeminiKey />
 
-      <DevPopover
-        place="bottom-start"
-        contentClick={false}
-        popButton={
-          <ReactTooltip tipData={"Projects"}>
-            <DevButton rounded="full" variant="v3" asIcon className="text-sm md:!block !hidden">
-              <IoApps className="text-3xl p-[6px]" />
-            </DevButton>
-          </ReactTooltip>
-        }
-      >
-        <PortfolioProjects />
-      </DevPopover>
+      {/* Removed portfolio popover */}
 
       <div>
         {userData ? (
