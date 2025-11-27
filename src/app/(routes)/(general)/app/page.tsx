6c30beac183eec3ef803eafed800e7e8362be75b
Nabil123-ops <dahdouhn74@gmail.com>
2025-11-27 23:00:09 +0200

    Update page.tsx

--- src/app/(routes)/(general)/app/page.tsx
+++ src/app/(routes)/(general)/app/page.tsx
@@ -1,18 +1,16 @@
-import { auth } from "@/auth";
-import HomeCards from "@/components/temp-components/home-cards";
 import React from "react";
+import HomeCards from "@/components/temp-components/home-cards";
 
-const page = async () => {
-  const session = await auth();
+const Page = () => {
 
   return (
     <section className="mt-5 w-full max-w-4xl mx-auto md:p-10 p-5">
       <h2 className="inline-block bg-gradient-to-r from-[#4E82EE] to-[#D96570] bg-clip-text md:text-5xl text-4xl text-transparent font-medium">
-        Hello, {session?.user ? session.user.name?.split(" ")[0] : "Guest"}
+        Hello, Guest
       </h2>
 
       <h3 className="md:text-5xl text-4xl text-wrap text-accentGray/50">
-        {session?.user ? "How can I help you today?" : "Sign in to get started"}
+        Sign in to get started
       </h3>
 
       <HomeCards />
@@ -20,4 +18,4 @@ const page = async () => {
   );
 };
 
-export default page;
+export default Page;
